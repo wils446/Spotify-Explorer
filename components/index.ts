857fcc8fcd0c6affd90214ce1c9321aad8b6df81
wils446 <wilsonlimmmm@gmail.com>
2022-07-26 23:07:55 +0700

    Add Components

---
+++ components/index.ts
@@ -0,0 +1,8 @@
+import Navbar from "./Navbar";
+import Head from "./Head";
+import LoadingSpinner from "./LoadingSpinner";
+import SearchInput from "./SearchInput";
+import SearchResult from "./SearchResult";
+import TrackCard from "./TrackCard";
+
+export { Navbar, Head, LoadingSpinner, SearchInput, SearchResult, TrackCard };
