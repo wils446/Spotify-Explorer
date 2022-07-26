39fc7f2125f86ee2ffe4336adc99de08f25ef697
wils446 <wilsonlimmmm@gmail.com>
2022-07-26 23:03:39 +0700

    add env

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
+	env: {
+		SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+		SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
+	},
 };
 
 module.exports = nextConfig;
